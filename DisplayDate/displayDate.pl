813db806d3a49aec71f2f5b8fb2ce23758fd3525
vangrunsven <lee_vangrunsven@outlook.com>
2016-11-17 12:33:49 -0800

    Added a third print line

--- DisplayDate/displayDate.pl
+++ DisplayDate/displayDate.pl
@@ -2,4 +2,5 @@
  
 $datestring = localtime();
 print "Local date and time $datestring\n";
-print "Local date and time (second time;) $datestring\n";
+print "Local date and time (second time \;) $datestring\n";
+print "Local date and time (third time \;) $datestring\n";
